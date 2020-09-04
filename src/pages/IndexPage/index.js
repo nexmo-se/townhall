9ff0f3c09e44e439992bf00870ad7e8b918f41f8
Toni Kuschan <toni.kuschan@vonage.com>
2020-09-04 17:32:18 +0200

    add simple landing page

--- src/pages/IndexPage/index.js
+++ src/pages/IndexPage/index.js
@@ -6,25 +6,52 @@ import config from "config";
 import useStyles from "./styles";
 
 import Button from "../../components/Button";
+import VonageLogo from "components/VonageLogo";
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router";
 
-function IndexPage(){
+function IndexPage() {
+  const mStyles = useStyles();
+  const { push } = useHistory();
+
   return (
     <React.Fragment>
-      <div className={}>
-        <VonageLogo 
-            style={{ 
-              position: "absolute", 
-              bottom: 32, 
-              right: 32,
-              zIndex: 2 
-            }}
+      <div className={mStyles.container}>
+        <VonageLogo
+          style={{
+            position: "absolute",
+            bottom: 32,
+            right: 32,
+            zIndex: 2,
+          }}
         />
-        <br/>
+        <br />
         <h1>Welcome to the Vonage Townhall!</h1>
-        <p>Please use the links below to get to the pages for the appropriate roles.</p>
-        <br/>
+        <p>
+          Please use the links below to get to the pages for the appropriate
+          roles.
+        </p>
+        <br />
+        <Button
+          text="Participant"
+          onClick={() => {
+            push("/participant");
+          }}
+        />
+        <Button
+          text="Moderator"
+          onClick={() => {
+            push("/moderator");
+          }}
+        />
+        <Button
+          text="Presenter"
+          onClick={() => {
+            push("/presenter");
+          }}
+        />
       </div>
     </React.Fragment>
-  )
+  );
 }
 export default IndexPage;
--- src/pages/IndexPage/styles.js
+++ src/pages/IndexPage/styles.js
@@ -1,1 +1,9 @@
-
+// @flow
+import { makeStyles } from "@material-ui/styles";
+export default makeStyles(() => ({
+  container: {
+    width: "100%",
+    height: "100%",
+    padding: "20rem",
+  },
+}));
